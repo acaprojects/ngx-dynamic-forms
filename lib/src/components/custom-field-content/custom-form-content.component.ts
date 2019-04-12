ae709cc4156bec8a11678f9eaa24fe1ef8fa1bee
Alex Sorafumo <alex@yuion.net>
2019-04-12 16:19:53 +1000

    Add timer handler to custom form content component. Add check for flex attribute on dynamic form fields

--- lib/src/components/custom-field-content/custom-form-content.component.ts
+++ lib/src/components/custom-field-content/custom-form-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 
 import { ADynamicFormField } from '../dynamic-field.class';
@@ -8,13 +8,14 @@ import { ADynamicFormField } from '../dynamic-field.class';
     template: ``,
     styles: [``]
 })
-export class CustomFormFieldContentComponent implements OnInit {
+export class CustomFormFieldContentComponent implements OnInit, OnDestroy {
     protected control: FormControl;
     protected ref_controls: { [name: string]: FormControl };
+    protected timers: { [name: string]: number } = {};
 
     constructor(protected field: ADynamicFormField<any>, protected group: FormGroup) { }
 
-    ngOnInit(): void {
+    public ngOnInit(): void {
         this.control = this.field.control as FormControl;
         if (this.field.references) {
             for (const ref of this.field.references) {
@@ -28,10 +29,21 @@ export class CustomFormFieldContentComponent implements OnInit {
         this.control.valueChanges.subscribe((v) => this.onChange(this.field.key, v));
     }
 
+    public ngOnDestroy(): void {
+        if (this.timers) {
+            for (const key in this.timers) {
+                if (this.timers.hasOwnProperty(key) && this.timers[key]) {
+                    clearTimeout(this.timers[key]);
+                    this.timers[key] = null;
+                }
+            }
+        }
+    }
+
     /**
      * Get formatted display value of the field value
      */
-    public get format() {
+    public get format(): string {
         return this.field.formatted();
     }
 
@@ -40,15 +52,28 @@ export class CustomFormFieldContentComponent implements OnInit {
      * @param key Key of the changed value
      * @param value New value
      */
-    protected onChange<T = any>(key: string, value: T) { }
+    protected onChange<T = any>(key: string, value: T): void { }
 
     /**
      * Get reference with the given index
      * @param index Index of the reference
      */
-    protected ref(index: number) {
+    protected ref(index: number): FormControl {
         return this.field.references && this.field.references.length > index
             ? this.ref_controls[this.field.references[index]]
             : null;
+    }
+
+    public timeout(name: string, fn: () => void, delay: number = 300): void {
+        this.clearTimer(name);
+        if (!(fn instanceof Function)) { return; }
+        this.timers[name] = <any>setTimeout(() => fn(), delay);
+    }
+
+    public clearTimer(name: string): void {
+        if (this.timers[name]) {
+            clearTimeout(this.timers[name]);
+            this.timers[name] = null;
+        }
     }
 }
